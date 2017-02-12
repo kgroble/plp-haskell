08ac22f18d2ddb3e3321e562e83b968f7aa182b8
Coleman <gibsonjc@rose-hulman.edu>
2017-02-11 19:33:14 -0500

    Added stuff

--- lib/ExerciseSolutions.hs
+++ lib/ExerciseSolutions.hs
@@ -4,18 +4,18 @@ data MyList a = Empty | MyList a (MyList a)
 
 fromList :: [a] -> MyList a
 fromList [] = Empty
-fromList (x:tail) = MyList x (fromList tail)
+fromList (x:rest) = MyList x (fromList rest)
 -------------------------------------------------
 printList :: (Show a) => MyList a -> String
 printList aList = "[" ++ (printListHelper aList "") ++ "]"
 
 printListHelper :: (Show a) => MyList a -> String -> String
 printListHelper Empty soFar = soFar
-printListHelper (MyList x tail) soFar = printListHelper tail (soFar ++ " " ++ (show x))
+printListHelper (MyList x rest) soFar = printListHelper rest (soFar ++ " " ++ (show x))
 -------------------------------------------------
 myLength :: MyList a -> Int
 myLength Empty = 0
-myLength (MyList _ tail) = 1 + myLength tail
+myLength (MyList _ rest) = 1 + myLength rest
 -------------------------------------------------
 myReverse :: MyList a -> MyList a
 myReverse Empty = Empty
@@ -23,7 +23,7 @@ myReverse aList = myReverseHelper aList Empty
 
 myReverseHelper :: MyList a -> MyList a -> MyList a
 myReverseHelper Empty soFar = soFar
-myReverseHelper (MyList x tail) soFar = myReverseHelper tail (MyList x soFar)
+myReverseHelper (MyList x rest) soFar = myReverseHelper rest (MyList x soFar)
 -------------------------------------------------
 mySort1 :: (Ord a) => MyList a -> MyList a
 mySort1 aList =
@@ -32,28 +32,26 @@ mySort1 aList =
     in fromList sorted
 
 toList :: MyList a -> [a]
-toList aList = toListHelper aList []
+toList Empty = []
+toList (MyList f rest) = (f:toList rest)
 
-toListHelper :: MyList a -> [a] -> [a]
-toListHelper Empty soFar = soFar
-toListHelper (MyList x tail) soFar = toListHelper tail (soFar ++ [x])
 -------------------------------------------------
 mySort2 :: (Ord a) => MyList a -> MyList a
 mySort2 aList = sortHelper aList Empty
 
 sortHelper :: (Ord a) => MyList a -> MyList a -> MyList a
 sortHelper Empty soFar = soFar
-sortHelper (MyList x tail) soFar = sortHelper tail (insertSorted x soFar)
+sortHelper (MyList x rest) soFar = sortHelper rest (insertSorted x soFar)
 
 insertSorted :: (Ord a) => a -> MyList a -> MyList a
 insertSorted x Empty = MyList x Empty
-insertSorted x (MyList y tail) =
+insertSorted x (MyList y rest) =
     if x < y then
-        MyList x (MyList y tail)
+        MyList x (MyList y rest)
     else
-        MyList y (insertSorted x tail)
+        MyList y (insertSorted x rest)
 -------------------------------------------------
-main = 
+main =
     --let x = MyList 2 (MyList 1 (MyList 3 Empty))
     let x = fromList [2, 1, 3, 4, 8, 6, 4]
     in putStrLn $  "Original: " ++ (printList x)
