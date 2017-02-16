95c21535da28a45c78080276cfda37a42c0662d4
Coleman <gibsonjc@rose-hulman.edu>
2017-02-16 00:21:08 -0500

    Added HTML example

--- lib/HTMLEscaping.hs
+++ lib/HTMLEscaping.hs
@@ -1,11 +1,16 @@
+
+
+
 module HTMLEscaping ( getString
                    , convertString )
 where
 
 data UnsafeString = Unsafe String
 
+-- escapes a string to be HTML safe
 convertString :: UnsafeString -> String
 convertString (Unsafe s) = s
 
-getString :: String -> UnsafeString
-getString s = Unsafe s
+-- some magic user input function
+getString :: UnsafeString
+getString = Unsafe "alert('I am an evil hacker');"
