f1c31bbee09097a935de1896ab96fb3678e70494
Coleman <gibsonjc@rose-hulman.edu>
2017-02-16 02:12:03 -0500

    Got file reading working

--- app/Main.hs
+++ app/Main.hs
@@ -1,65 +1,14 @@
-{-# LANGUAGE GADTs, KindSignatures, DataKinds #-}
-
 module Main where
 
-import HTMLEscaping
--- import RedBlackTree
-
-class Pretty a where
-  display :: a -> String
-
-data MyList a = Empty
-              | Something a (MyList a)
-
-instance (Pretty a) => Pretty (MyList a) where
-  display Empty = ""
-  display (Something a rest) = (display a) ++ (display rest)
+import LineSegment
+import qualified Data.Text as T
 
 
+getLineSeg :: String -> LineSeg
+getLineSeg = read
 
-instance Pretty Int where
-  display x = show x
 
 main :: IO ()
-main =
-  let
-    s = getString "Hello"
-  in putStrLn $ convertString s
-
-
-
-
-
-
-
-
-instance Pretty Integer where
-  display x = show x
-
-instance Pretty Float where
-  display x = show x
-
-instance Pretty Color where
-  display Red = "Red "
-  display Blue = "Blue "
-  display Green = "Green "
-  display White = "White "
-
-data Color = Red
-           | Blue
-           | Green
-           | White
-  -- putStrLn $ display $ Something Red $ Something Blue $ Something Green $ Something Red Empty
-
--- data Something = A | B | C
-
--- main :: IO ()
--- main =
---   let tree = Root (BTree (Empty) A (RTree (Empty) B (Empty)))
---       firstTest = contains tree A
---       another = contains tree B
---       ah = contains tree C
---   in
---     putStrLn (if (firstTest && another && (not ah))
---               then "All passed!!!!!"
---               else "damn")
+main = do
+  content <- readFile "input/line-segments"
+  putStrLn $ show $ map getLineSeg (lines content)
--- lib/LineSegment.hs
+++ lib/LineSegment.hs
@@ -2,9 +2,9 @@ module LineSegment ( Point ( Pt )
                    , LineSeg ( Line ) ) where
 
 data Point = Pt Int Int
-             deriving (Eq, Ord)
+             deriving (Eq, Ord, Read, Show)
 
 data LineSeg = Line
                Point
                Point
-             deriving (Eq, Ord)
+             deriving (Eq, Ord, Read, Show)
